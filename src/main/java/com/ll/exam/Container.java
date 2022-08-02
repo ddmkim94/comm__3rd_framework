de1fd58675b0098d9d74a201ebf62a428b2840a8
ddmkim94 <ddmkim94@gmail.com>
2022-08-02 15:22:58 +0900

    Repository 스캔 & Autowired

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -2,6 +2,7 @@ package com.ll.exam;
 
 import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.Repository;
 import com.ll.exam.annotation.Service;
 import org.reflections.Reflections;
 
@@ -17,10 +18,18 @@ public class Container {
     }
 
     private static void scanComponents() {
+        scanRepositories();
         scanServices();
         scanControllers();
 
         resolveDependenciesAllComponents();
+    }
+
+    private static void scanRepositories() {
+        Reflections ref = new Reflections(App.BASE_PACKAGE_PATH);
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Repository.class)) {
+            objects.put(cls, Ut.cls.newObj(cls, null));
+        }
     }
 
     private static void resolveDependenciesAllComponents() {
@@ -52,14 +61,14 @@ public class Container {
     }
 
     private static void scanServices() {
-        Reflections ref = new Reflections("com.ll.exam");
+        Reflections ref = new Reflections(App.BASE_PACKAGE_PATH);
         for (Class<?> cls : ref.getTypesAnnotatedWith(Service.class)) {
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
     }
 
     private static void scanControllers() {
-        Reflections ref = new Reflections("com.ll.exam");
+        Reflections ref = new Reflections(App.BASE_PACKAGE_PATH);
         for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
